d9cd401d200a530287e4013975741624640aae8d
Martin Ku <martin79831@gmail.com>
2012-10-04 08:53:10 +0800

    Added more test cases for Skype4Py.

--- test/Skype4Pytest.py
+++ test/Skype4Pytest.py
@@ -1,22 +1,55 @@
-import Skype4Py, time
-import win32com.client as comclt
+import Skype4Py, time, sys, platform
 
-# Create Skype instance
-skype = Skype4Py.Skype()
+"""
+Main purpose of (2)(3)(4) is about typing indicator.
+Test case:
+  1. Attach running Skype
+  2. OpenMessageDialog
+  3. Type somethings
+  4. Close Message Dialog
+"""
+if __name__ == "__main__":
+ osPlatform = platform.system()
+  print 'Your platform is %s' % osPlatform
+  if osPlatform=="Windows":
+    import win32com.client as comclt
+  elif osPlatform=="Linux":
+    import uinput
+  else:
+    print 'Sorry your we do not have implementation on %s' % osPlatform
+    sys.exit(0)
 
-# Connect Skype object to Skype client
-skype.Attach()
+  # Create Skype instance
+  skype = Skype4Py.Skype(Transport='x11') if osPlatform=="Linux" else Skype4Py.Skype()
 
-print 'Your full name:', skype.CurrentUser.FullName
-print 'Your contacts:'
-for user in skype.Friends:
-  print '    ', user.FullName
+  # (1) Connect Skype object to Skype client
+  skype.Attach()
 
-skype.Client.OpenMessageDialog('martin02588520')
-#skype.Client.ButtonPressed('A')
-#skype.Client.ButtonReleased('A')
-wsh = comclt.Dispatch("WScript.Shell")
-##wsh.AppActivate("Notepad") # select another application
-for i in range(50):
-  time.sleep(0.1)
-  wsh.SendKeys(str(i)) # send the keys you want
+  print 'Your full name:', skype.CurrentUser.FullName
+  print 'Your contacts:'
+  for user in skype.Friends:
+    print '    ', user.FullName
+
+  # (2)
+  skype.Client.OpenMessageDialog('echo123')
+  if osPlatform=="Windows":
+    wsh = comclt.Dispatch("WScript.Shell")
+    wsh.SendKeys('...') # send the keys you want
+  elif osPlatform=="Linux":
+    # Check /usr/include/linux/input.h for more definitions
+    events = (uinput.KEY_DOT, uinput.KEY_LEFTALT, uinput.KEY_F4)
+    device = uinput.Device(events)
+    # (3)
+    device.emit(uinput.KEY_DOT, 1)
+    device.emit(uinput.KEY_DOT, 0)
+    device.emit(uinput.KEY_BACKSPACE, 1)
+    device.emit(uinput.KEY_BACKSPACE, 0)
+    # (4)
+    device.emit(uinput.KEY_LEFTALT, 1)
+    device.emit(uinput.KEY_F4, 1)
+    device.emit(uinput.KEY_F4, 0)
+    device.emit(uinput.KEY_LEFTALT, 0)
+    time.sleep(3) # we need this to let input completetly be sent to kernel
+  else:
+    # Should never reach here
+    pass
