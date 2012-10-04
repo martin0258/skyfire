c826ada431d80146457b73a3d16768e6ba4def54
Martin Ku <martin79831@gmail.com>
2012-10-04 13:59:14 +0800

    Fixed text cases of typing indicator in Skype. #9

--- test/Skype4Pytest.py
+++ test/Skype4Pytest.py
@@ -26,33 +26,35 @@ if __name__ == "__main__":
   skype.Attach()
  
   print 'Your full name:', skype.CurrentUser.FullName
-  print 'Your contacts:'
-  for user in skype.Friends:
-    print '    ', user.FullName
+  print 'Your Skype name:', skype.CurrentUserHandle
  
   # (2)
-  skype.CreateChatWith('echo123').OpenWindow()
+  time.sleep(3)
+  skype.CreateChatWith('skysourcefire').OpenWindow()
   # Following method would get Skype4Py.errors.SkypeError: [Errno 7] in Ubuntu 12.04
   # skype.Client.OpenMessageDialog('echo123')
   if osPlatform=="Windows":
     wsh = comclt.Dispatch("WScript.Shell")
-    wsh.SendKeys('...') # send the keys you want
+    wsh.SendKeys('..................') # trigger typing indicator
+    time.sleep(3) # If you close the window too quick, you'll not see the typing indicator
+    wsh.SendKeys('^a{DEL}%{F4}') # clear text and close chat window
   elif osPlatform=="Linux":
+    # Pre-Condition: sudo apt-get install xdotool
+    windowId = subprocess.check_output(['xdotool', 'getactivewindow'])
+    windowName = subprocess.check_output(['xdotool', 'getwindowname', windowId ])
+    assert "Skype" in windowName, "Wrong focus window [%s]. Should be Skype." % windowName
+    subprocess.check_output(['xdotool', 'key', 'a', 'ctrl+a', 'BackSpace', 'alt+F4'])
     # Check /usr/include/linux/input.h for more definitions
     # Note: You may need to 'sudo' if you get 'OSError: [Errno 13] Permission denied'
-    events = (uinput.KEY_DOT, uinput.KEY_LEFTALT, uinput.KEY_F4)
-    device = uinput.Device(events)
-    # (3)
-    device.emit(uinput.KEY_DOT, 1)
-    device.emit(uinput.KEY_DOT, 0)
-    device.emit(uinput.KEY_BACKSPACE, 1)
-    device.emit(uinput.KEY_BACKSPACE, 0)
-    # (4)
-    device.emit(uinput.KEY_LEFTALT, 1)
-    device.emit(uinput.KEY_F4, 1)
-    device.emit(uinput.KEY_F4, 0)
-    device.emit(uinput.KEY_LEFTALT, 0)
-    time.sleep(3) # we need this to let input completetly be sent to kernel
+    # TODO: The following method only works sometimes. Don't know why.
+    #events = (uinput.KEY_H, uinput.KEY_LEFTALT, uinput.KEY_F4)
+    #device = uinput.Device(events)
+    #device.emit(uinput.KEY_H, 1)
+    #device.emit(uinput.KEY_H, 0)
+    #device.emit(uinput.KEY_LEFTALT, 1)
+    #device.emit(uinput.KEY_F4, 1)
+    #device.emit(uinput.KEY_F4, 0)
+    #device.emit(uinput.KEY_LEFTALT, 0)
   else:
     # Should never reach here
     pass
